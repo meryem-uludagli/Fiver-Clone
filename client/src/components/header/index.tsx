26cf29f2f2e368b231ac1a9ff19b1753331d3f74
Meryem Uludağlı <meryemuslu2035@gmail.com>
2025-04-29 15:25:22 +0300

    added

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
+import User from "./user";
+import Links from "./links";
 
 const Header = () => {
+  const user = false;
   return (
     <header className="p-5 shadow">
       <div className="max-w-[1440px] mx-auto flex justify-between gap-4 md:gap-8">
@@ -10,11 +13,18 @@ const Header = () => {
         </Link>
 
         <form className="flex-1 flex border rounded overflow-hidden max-w[500px]">
-          <input type="text" className="w-full h-full px-3 outline-none" />
-          <button className="bg-black p-2 text-white text-xl">
+          <input
+            type="text"
+            className="w-full h-full px-3 outline-none"
+            placeholder="hizmet ara..."
+          />
+          <button className="bg-black p-2 text-white text-xl max-sm:hidden">
             <IoSearch />
           </button>
         </form>
+        <div className="flex items-center gap-2 relative group">
+          {user ? <User /> : <Links />}
+        </div>
       </div>
     </header>
   );
---
+++ client/src/components/header/links.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Links = () => {
+  return (
+    <>
+      <Link to="/login" className="transitionhover:text-green-500">
+        Giriş Yap
+      </Link>
+      <Link
+        to="/register"
+        className="transition border rounded border-green-500 p-1 hover:bg-green-500 hover:text-white"
+      >
+        Kaydol
+      </Link>
+    </>
+  );
+};
+export default Links;
