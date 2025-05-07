107b979e0e9a8ba90219d6ea1449314ddc4add6c
Meryem Uludağlı <meryemuslu2035@gmail.com>
2025-05-07 13:45:54 +0300

    add

--- client/src/components/protected/index.tsx
+++ client/src/components/protected/index.tsx
@@ -3,16 +3,10 @@ import { useAuth } from "../../context/authContent";
 import Loader from "../loader";
 
 const Protected = () => {
-  // kullanıcı verilerini al
   const { user, isLoading } = useAuth();
-
-  // kullanıcı verisi yüklenene kadar loader bas
   if (isLoading) return <Loader designs="my-20 size-10" />;
 
-  // kullanıcı hesabı satıcı değilse anasayfaya yönlendir
   if (!user?.isSeller) return <Navigate to="/" replace />;
-
-  // kullanıcı hesabı satıcıysa sayfayı göster
   return <Outlet />;
 };
 
